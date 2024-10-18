b1d61b7a545d7c5593cd91fe93caed2e96022c76
OTIS <72382663+1domybest@users.noreply.github.com>
2024-10-18 16:24:27 +0900

    add method

--- Sources/OtisLogManagerLibrary/OtisLogManagerLibrary.swift
+++ Sources/OtisLogManagerLibrary/OtisLogManagerLibrary.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import LogManager
+
+open class OtisLogManagerLibrary {
+    public init(projectName: String) {
+        LogManager(projectName: projectName)
+    }
+}
